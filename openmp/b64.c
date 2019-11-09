1cabae63ed6b71cdfca4f685e699e3de0fe69b6e
Jacob Williams <jacob3.williams@live.uwe.ac.uk>
2019-11-09 15:22:18 +0000

    testing & spring cleaning

--- openmp/b64.c
+++ openmp/b64.c
@@ -6,19 +6,20 @@
 size_t calcDecodeLength(char* b64input) {
     size_t len = strlen(b64input), padding = 0;
 
-    if (b64input[len-1] == '=' && b64input[len-2] == '=') //last two chars are =
+    if (b64input[len-1] == '=' && b64input[len-2] == '='){
         padding = 2;
-    else if (b64input[len-1] == '=') //last char is =
+    }else if (b64input[len-1] == '='){
         padding = 1;
+    }
+
     return (len*3)/4 - padding;
 }
 
 void Base64Decode( char* b64message, unsigned char** buffer, size_t* length) {
-
-    
-    BIO *bio, *b64;  // A BIO is an I/O strean abstraction
+    BIO *bio, *b64;
 
     int decodeLen = calcDecodeLength(b64message);
+
     *buffer = (unsigned char*)malloc(decodeLen + 1);
     (*buffer)[decodeLen] = '\0';
 
@@ -26,7 +27,6 @@ void Base64Decode( char* b64message, unsigned char** buffer, size_t* length) {
     b64 = BIO_new(BIO_f_base64());
     bio = BIO_push(b64, bio);
 
-    //BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Do not use newlines to flush buffer
     *length = BIO_read(bio, *buffer, strlen(b64message));
     BIO_free_all(bio);
 }
--- openmp/main.c
+++ openmp/main.c
@@ -8,14 +8,11 @@
 #include "b64.c"
 #include "aes.c"
 
-//916132832
-#define sp 14776336
-
 int success = 0;
 void printTime(clock_t, clock_t);
 
 int checkPlaintext(char* plaintext, char* result){
-    int length = 10; // we just check the first then characters
+    int length = 10; 
     return strncmp(plaintext, result, length);
 }
 
@@ -23,24 +20,23 @@ int main (void)
 {
 
     clock_t start = clock(), end;
-    // This is the string Hello, World! encrypted using aes-256-cbc with the
-    // pasword 12345
     char* ciphertext_base64 = (char*) "U2FsdGVkX19VjPGO9qgNMHQCCUycG42mf7Ak0JMI79lPmAAu8XCmJfY4T"
                                         "/8T2RLDrnsf9WVPPGqB/rVgfRMhDmLnNsgp1Ukh8ygs+j0cgCYO4O3J"
                                         "5EMVb7utga9xSFSXe0ZsrfngA+ftf4OL6jOioA==\n";
     char* plaintext = "This is the top seret message in parallel computing!"
                         "Please keep it in a safe place.";
+    // pasword 12Dec
+
     char dict[] = "0123456789"
                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                     "abcdefghijklmnopqrstuvwxyz";
     //char dict[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                    //"abcdefghijklmnopqrstuvwxyz"
-                    //"0123456789";
+    //                "abcdefghijklmnopqrstuvwxyz"
+    //                "0123456789";
     
     int decryptedtext_len, ciphertext_len, dict_len;
 
-    // cipher (binary) pointer and length
-    size_t cipher_len; // size_t is sizeof(type)
+    size_t cipher_len;
     unsigned char* ciphertext;
   
     unsigned char salt[8];
@@ -56,7 +52,6 @@ int main (void)
     unsigned char* password = &plainpassword[0];
     int password_length = 3;
 
-    // retrive the slater from ciphertext (binary)
     if (strncmp((const char*)ciphertext,"Salted__",8) == 0) {
         
         memcpy(salt,&ciphertext[8],8);
@@ -67,14 +62,9 @@ int main (void)
 
     dict_len = strlen(dict);
     
-    // generate key and iv
-    //printf("%s", plaintext);
     omp_set_num_threads(4);
 
-    //#pragma omp parallel
-    //{
         int id;
-        //int numofthreads = omp_get_num_threads();
         #pragma omp parallel for collapse(5)
         for(int i = 0; i < dict_len; i++)
             for(int j = 0; j < dict_len; j++)
